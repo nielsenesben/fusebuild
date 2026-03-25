e86a8093bcc52cd8ef0298c504212e2e3491b316
Esben Nielsen <nielsen.esben@gmail.com>
2026-03-25 11:12:38 +0100

    Running all tests in test/core automatically under fusebuild.

--- test/core/FUSEBUILD.py
+++ test/core/FUSEBUILD.py
@@ -1,44 +1,20 @@
+import glob
+
 from fusebuild import BwrapSandbox, NoSandbox, get_action, shell_action
 from fusebuild.python import mypy_actions, pyc_actions, pyc_mappings
 
 pyc_actions()
 mypy_actions()
 
-shell_action(
-    name="test_utils",
-    cmd="\n".join(
-        [
-            ". $OUTPUT_DIR/../../../venv/bin/activate",
-            "python -B test_utils.py",
-        ]
-    ),
-    category="test",
-    mappings=pyc_mappings(),
-)
-
-shell_action(
-    name="test_libfusebuild",
-    cmd="\n".join(
-        [
-            ". $OUTPUT_DIR/../../../venv/bin/activate",
-            "python -B test_libfusebuild.py",
-        ]
-    ),
-    category="test",
-    mappings=pyc_mappings(),
-)
-
-shell_action(
-    name="test_graph",
-    cmd="\n".join(
-        [
-            ". $OUTPUT_DIR/../../../venv/bin/activate",
-            "export FUSEBUILD_CACHE_DIR=$OUTPUT_DIR/fusebuild_cache",
-            "ls -altr",
-            "python -B test_graph.py",
-            "ls -altr",
-        ]
-    ),
-    category="test",
-    mappings=pyc_mappings(),
-)
+for test in glob.glob("test_*.py"):
+    shell_action(
+        name=test[0:-3],
+        cmd="\n".join(
+            [
+                ". $OUTPUT_DIR/../../../venv/bin/activate",
+                f"python {test}",
+            ]
+        ),
+        category="test",
+        mappings=pyc_mappings(),
+    )
